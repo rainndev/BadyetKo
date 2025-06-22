386796d103692e4e1b16f0ad2fe0cb91a56b4d81
rainndev <182114243+rainndev@users.noreply.github.com>
2025-06-22 22:53:04 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-full h-dvh flex items-center justify-center bg-[#212121] text-amber-300">
+      BadyetKo
+    </div>
+  );
+};
+
+export default App;
