fd5fba77c524437de945e22b01cb750cb55cac4f
rainndev <182114243+rainndev@users.noreply.github.com>
2025-07-06 21:39:30 +0800

    feat: implement logout functionality in ProfilePage and clean up NavigationBar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,11 +1,9 @@
 import { useSession } from "../context/SessionContext";
 import { NavLink } from "react-router-dom";
-import supabase from "../supabase/supabase-client";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
 import { HiBanknotes } from "react-icons/hi2";
 import { TbSettings2 } from "react-icons/tb";
 import { MdOutlineAccountCircle } from "react-icons/md";
-import { RiLogoutCircleLine } from "react-icons/ri";
 
 const navData = [
   {
@@ -45,33 +43,24 @@ const navData = [
 const NavigationBar = () => {
   const { session } = useSession();
 
-  const handleSignout = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) throw error;
-  };
-
   return (
     session !== null && (
-      <div className="bg-light-background md:bg-medium-light-background fixed right-0 bottom-0 left-0 z-1 flex flex-col items-center justify-center border-2 border-t p-3 md:static md:z-0 md:min-h-screen md:border-none md:p-10">
+      <div className="bg-light-background md:bg-medium-light-background fixed right-0 bottom-0 left-0 z-1 flex flex-col items-center justify-center border-2 border-t p-3 md:static md:z-0 md:min-h-screen md:border-none md:p-8">
         <HiBanknotes className="my-15 hidden text-2xl md:block" />
 
-        <ul className="text-dark-txt/50 flex h-full w-full items-center justify-between gap-10 px-5 text-xl md:flex-col md:justify-normal md:px-0">
+        <ul className="text-dark-txt/50 flex h-full w-full items-start justify-between gap-5 px-5 text-xl md:flex-col md:justify-normal md:px-0">
           {navData.map((navItem) => (
             <NavLink
               key={navItem.name}
-              className="cursor-pointer"
+              className="hover:bg-dark-background/10 flex w-full cursor-pointer items-center justify-start gap-2 rounded-sm px-5 py-2"
               to={navItem.path}
             >
               <li>{navItem.icon}</li>
+              <li className="hidden text-[clamp(.6rem,2vw+.6rem,.9rem)] md:block">
+                {navItem.name}
+              </li>
             </NavLink>
           ))}
-
-          <li
-            onClick={() => handleSignout()}
-            className="text-light-background bg-dark-background rounded-sm p-2.5 md:p-3"
-          >
-            <RiLogoutCircleLine />
-          </li>
         </ul>
       </div>
     )
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,7 +1,24 @@
+import supabase from "@/supabase/supabase-client";
+import { RiLogoutCircleLine } from "react-icons/ri";
+
 const ProfilePage = () => {
+  const handleSignout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) throw error;
+  };
+
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      ProfilePage
+    <div className="flex h-screen w-full items-start justify-start p-10">
+      {/* logout button */}
+      <div
+        onClick={() => handleSignout()}
+        className="text-light-background bg-dark-background hover:bg-dark-background/90 flex cursor-pointer items-center gap-2 rounded-sm p-2.5 md:p-3"
+      >
+        <span>
+          <RiLogoutCircleLine />
+        </span>
+        <span className="text-[clamp(.6rem,2vw+.6rem,.9rem)]">Log out</span>
+      </div>
     </div>
   );
 };
