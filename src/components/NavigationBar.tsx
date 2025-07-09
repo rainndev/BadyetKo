3ac4d345aca6884b36e4ebdf3019dc644062528b
rainndev <182114243+rainndev@users.noreply.github.com>
2025-07-09 23:24:25 +0800

    feat: replace HiBanknotes icon with TransparentLogo in NavigationBar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,7 +1,7 @@
+import TransparentLogo from "@/assets/logos/transaprent-logo-w-text.png";
 import { useSession } from "../context/SessionContext";
 import { NavLink } from "react-router-dom";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
-import { HiBanknotes } from "react-icons/hi2";
 import { TbSettings2 } from "react-icons/tb";
 import { MdOutlineAccountCircle } from "react-icons/md";
 
@@ -46,7 +46,11 @@ const NavigationBar = () => {
   return (
     session !== null && (
       <div className="bg-light-background md:bg-medium-light-background fixed right-0 bottom-0 left-0 z-1 flex flex-col items-center justify-center border-2 border-t p-3 md:static md:z-0 md:min-h-screen md:border-none md:p-8">
-        <HiBanknotes className="my-15 hidden text-2xl md:block" />
+        <img
+          src={TransparentLogo}
+          className="my-10 hidden max-w-35 md:block lg:max-w-40"
+          alt="Transaparent logo with text"
+        />
 
         <ul className="text-dark-txt/50 flex h-full w-full items-start justify-between gap-5 px-5 text-xl md:flex-col md:justify-normal md:px-0">
           {navData.map((navItem) => (
