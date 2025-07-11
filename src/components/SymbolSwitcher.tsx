96b01f255491ffe724ed98f3dc8b8b609fcc55b5
rainndev <182114243+rainndev@users.noreply.github.com>
2025-07-11 11:00:04 +0800

    style: refactor SymbolSwitcher layout and improve switch label text

--- src/components/SymbolSwitcher.tsx
+++ src/components/SymbolSwitcher.tsx
@@ -6,15 +6,27 @@ export function SymbolSwitcher() {
   const setISOSymbol = useCurrencyStore((state) => state.setISOSymbol);
 
   return (
-    <div className="mt-5 flex items-center space-x-2">
-      <Switch
-        onCheckedChange={() => setISOSymbol(!isISOSymbol)}
-        checked={isISOSymbol}
-        id="currency-symbol-switcher"
-      />
-      <label htmlFor="currency-symbol-switcher">
-        {isISOSymbol ? "Disable" : "Enable"} ISO Symbol
-      </label>
+    <div className="bg-dark-background/5 mt-5 flex flex-wrap items-center justify-between gap-5 rounded-2xl p-6">
+      <div className="min-w-40 flex-1">
+        <h1 className="text-[clamp(.9rem,2vw+.9rem,1rem)] font-semibold">
+          Currency Format Toggle
+        </h1>
+        <p className="text-muted-foreground text-[clamp(.5rem,2vw+.5rem,0.875rem)]">
+          Switch between using the currency symbol (₱) and the ISO code (PHP)
+          when displaying amounts.
+        </p>
+      </div>
+      <div className="flex shrink-0 items-center gap-2">
+        <Switch
+          className="!text-[clamp(.9rem,2vw+.9rem,1rem)]"
+          onCheckedChange={() => setISOSymbol(!isISOSymbol)}
+          checked={isISOSymbol}
+          id="currency-symbol-switcher"
+        />
+        <label className="text-nowrap" htmlFor="currency-symbol-switcher">
+          {isISOSymbol ? "Enabled" : "Disabled"}
+        </label>
+      </div>
     </div>
   );
 }
