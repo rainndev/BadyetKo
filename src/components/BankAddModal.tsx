74085a0a9c51336cecfd03857c8417b0fe3c8991
rainndev <182114243+rainndev@users.noreply.github.com>
2025-07-17 20:38:51 +0800

    feat: update title font and add animation for adding bank modal

--- src/components/BankAddModal.tsx
+++ src/components/BankAddModal.tsx
@@ -3,25 +3,22 @@ import { useCreateAvatar } from "@/queries/useCreateAvatar";
 import { addBankSchema } from "@/schemas/banks.schema";
 import { IoMdClose } from "react-icons/io";
 import { useEffect, useRef, useState } from "react";
-import { useBodyScrollLock } from "@/hooks/useBodyScrollLock";
 import { useSession } from "@/context/SessionContext";
 import CustomBankAvatar from "@/components/CustomBankAvatar";
+import TransparentLogoOnly from "@/assets/logos/transparent-logo-only.png";
+import { easeInOut, motion } from "framer-motion";
 
 interface BankAddModalProps {
-  isShowModal: boolean;
   setShowModal: (show: boolean) => void;
 }
 
-const BankAddModal = ({ isShowModal, setShowModal }: BankAddModalProps) => {
+const BankAddModal = ({ setShowModal }: BankAddModalProps) => {
   const [bankName, setBankName] = useState("");
   const [image, setImage] = useState<File | null>(null);
   const [errorMessage, setErrorMessage] = useState("");
   const imageRef = useRef<HTMLInputElement>(null);
   const { userID } = useSession();
   const { addBank, isAddBankPending, isAddBankSuccess } = useBank();
-
-  //Lock the body when showing modal
-  useBodyScrollLock(isShowModal);
 
   //ADD NEW CUSTOM AVATAR
   const { mutate: addAvatar } = useCreateAvatar(image);
@@ -71,56 +68,80 @@ const BankAddModal = ({ isShowModal, setShowModal }: BankAddModalProps) => {
   }, [isAddBankSuccess, isAddBankPending]);
 
   return (
-    isShowModal && (
-      <div className="bg-dark-background/90 fixed z-20 flex h-dvh w-screen items-center justify-center backdrop-blur-xs">
-        <div className="bg-light-background text-dark-txt mx-3 flex w-full max-w-xl flex-col items-center justify-center rounded-2xl p-3">
-          <div className="border-dark-txt/10 flex w-full justify-between border-b-2 p-5 md:p-10">
-            <h1 className="text-fluid-xl">Add New accounts</h1>
-            <button
-              className="text-dark-txt/50 text-fluid-2xl cursor-pointer"
-              disabled={isAddBankPending}
-              onClick={() => {
-                setErrorMessage("");
-                setShowModal(false);
-              }}
-            >
-              <IoMdClose />
-            </button>
+    <div className="bg-dark-background/90 fixed z-20 flex h-dvh w-screen items-center justify-center backdrop-blur-xs">
+      <motion.div
+        initial={{
+          scale: 0.95,
+          opacity: 0,
+        }}
+        animate={{
+          scale: 1,
+          opacity: 1,
+        }}
+        exit={{
+          scale: 0.95,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 0.25,
+          ease: "easeInOut",
+        }}
+        className="bg-light-background text-dark-txt mx-3 flex w-full max-w-md flex-col items-center justify-center rounded-2xl p-3"
+      >
+        <div className="border-dark-txt/10 flex w-full justify-between p-5 pb-0">
+          <div className="flex items-center gap-2">
+            <div className="text-fluid-lg w-fit rounded-full border p-1">
+              <img src={TransparentLogoOnly} className="w-6" />
+            </div>
+
+            <h1 className="text-fluid-lg font-inter w-full text-start font-semibold">
+              Add New Account
+            </h1>
           </div>
 
-          <div className="w-full p-5 md:p-10">
-            <form
-              className="relative space-y-2 rounded-2xl"
-              onSubmit={handleSubmit}
-            >
-              <CustomBankAvatar
-                setErrorMessage={setErrorMessage}
-                setImageCropped={setImage}
-              />
-              <p className="text-dark-txt/90 text-fluid-lg mb-2">Bank Name</p>
-
-              <input
-                type="text"
-                value={bankName}
-                placeholder="e.g. Example Bank"
-                className="ring-dark-background/10 focus:ring-dark-background text-fluid-sm w-full rounded-lg p-3 ring transition duration-300 ease-in-out focus:ring-2 focus:ring-offset-1 focus:outline-none"
-                onChange={(e) => setBankName(e.target.value)}
-              />
-              <p className="mb-2 text-sm text-red-400">{errorMessage}</p>
-
-              <div className="grid w-full grid-cols-1 items-center justify-between gap-2 sm:grid-cols-2">
-                <button
-                  disabled={isAddBankPending}
-                  className="bg-dark-background text-light-background hover:bg-dark-background/90 text-fluid-sm cursor-pointer rounded-lg p-3 px-6 transition-colors ease-in-out"
-                >
-                  {isAddBankPending ? "Loading..." : "Add Account"}
-                </button>
-              </div>
-            </form>
-          </div>
+          <button
+            className="text-dark-txt/50 text-fluid-xl cursor-pointer"
+            disabled={isAddBankPending}
+            onClick={() => {
+              setErrorMessage("");
+              setShowModal(false);
+            }}
+          >
+            <IoMdClose />
+          </button>
         </div>
-      </div>
-    )
+
+        <div className="w-full p-5">
+          <form
+            className="relative space-y-1 rounded-2xl"
+            onSubmit={handleSubmit}
+          >
+            <CustomBankAvatar
+              setErrorMessage={setErrorMessage}
+              setImageCropped={setImage}
+            />
+
+            <p className="text-dark-txt/90 text-fluid-base mb-2">Bank Name</p>
+
+            <input
+              type="text"
+              value={bankName}
+              placeholder="e.g. Example Bank"
+              className="ring-dark-background/10 focus:ring-dark-background text-fluid-sm w-full rounded-lg p-3 ring transition duration-300 ease-in-out focus:ring-2 focus:ring-offset-1 focus:outline-none"
+              onChange={(e) => setBankName(e.target.value)}
+            />
+            <p className="text-fluid-sm mb-2 text-red-400">{errorMessage}</p>
+
+            <button
+              disabled={isAddBankPending}
+              className="bg-dark-background text-light-background hover:bg-dark-background/90 text-fluid-sm w-full cursor-pointer rounded-lg p-3 px-6 transition-colors ease-in-out"
+            >
+              {isAddBankPending ? "Loading..." : "Add Account"}
+            </button>
+          </form>
+        </div>
+      </motion.div>
+    </div>
   );
 };
 
