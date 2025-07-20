42b23016819c808fbdfb714e2c08a295750c7de7
rainndev <182114243+rainndev@users.noreply.github.com>
2025-07-20 13:09:35 +0800

    feat: enhance CustomCardHeader to display spending tips based on currency

--- src/components/CustomCardHeader.tsx
+++ src/components/CustomCardHeader.tsx
@@ -4,11 +4,14 @@ import { useSpendingTip } from "@/hooks/useSpendingTips";
 import { useCategoryPieData } from "@/queries/useCategoryPieData";
 import type { ChartPieDonutData } from "@/api/user";
 import { hexToRgba } from "@/utils/helper";
+import { useCurrencyStore } from "@/store/CurrencyStore";
 
 const CustomCardHeader = () => {
   const { data, isLoading } = useCategoryPieData();
   const [mostSpendCategory, setMostSpendCategory] =
     useState<ChartPieDonutData | null>();
+  const isPH =
+    useCurrencyStore((state) => state.currencyOptions.currency) === "PHP";
 
   const isDataEmpty = !data || data.length === 0;
 
@@ -39,30 +42,47 @@ const CustomCardHeader = () => {
 
   return (
     <CardHeader className="items-center pb-0">
-      <CardTitle>Saan ka na naman gumastos?</CardTitle>
-      <CardDescription className="mt-2">
-        <p className="text-fluid-base items-center leading-relaxed">
-          {parts[0]}
-          <span
-            key={mostSpendCategory?.category_name}
-            style={{
-              backgroundColor: hexToRgba(
-                mostSpendCategory?.color || "#f26f6f",
-                30,
-              ),
-              border: "1px solid",
-              borderColor: mostSpendCategory?.color || "#f26f6f",
-            }}
-            className="text-fluid-xs mx-1 inline-flex items-center gap-2 rounded-full px-4 py-0.5 font-medium"
-          >
-            <span className="text-dark-txt/90 text-nowrap">
-              {mostSpendCategory?.category_name}
-            </span>
-          </span>
+      <CardTitle>
+        {isPH ? "Saan ka na naman gumastos? " : "Where Did Your Money Go?"}
+      </CardTitle>
+      {isPH ? (
+        <CardDescription className="mt-2">
+          {isDataEmpty && (
+            <p>
+              Wala pa kaming maipakitang datos. Simulan mo nang i-track ang
+              iyong pera ngayon!
+            </p>
+          )}
 
-          {parts[1]}
-        </p>
-      </CardDescription>
+          {!isDataEmpty && (
+            <p className="text-fluid-base items-center leading-relaxed">
+              {parts[0]}
+              <span
+                key={mostSpendCategory?.category_name}
+                style={{
+                  backgroundColor: hexToRgba(
+                    mostSpendCategory?.color || "#f26f6f",
+                    30,
+                  ),
+                  border: "1px solid",
+                  borderColor: mostSpendCategory?.color || "#f26f6f",
+                }}
+                className="text-fluid-xs mx-1 inline-flex items-center gap-2 rounded-full px-4 py-0.5 font-medium"
+              >
+                <span className="text-dark-txt/90 text-nowrap">
+                  {mostSpendCategory?.category_name}
+                </span>
+              </span>
+
+              {parts[1]}
+            </p>
+          )}
+        </CardDescription>
+      ) : (
+        <CardDescription>
+          A breakdown of your deposits and withdrawals by category
+        </CardDescription>
+      )}
     </CardHeader>
   );
 };
